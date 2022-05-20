5d9327949ac3569626b69ecdfa441a09e2d8818d
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-05-20 19:26:33 -0300

    Projeto CRUDArq criado

---
+++ crudarq/src/main/java/com/wanzeler/crudarq/CrudarqApplication.java
@@ -0,0 +1,13 @@
+package com.wanzeler.crudarq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudarqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudarqApplication.class, args);
+	}
+
+}
